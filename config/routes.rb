2bf114bcaba0fdf8aa780a0fae73f90750c8d012
近藤正道 <kuros96@icloud.comgit config --global user.name masamichi0963>
2019-11-01 13:19:55 +0900

    edit-view

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
-  root 'groups#index'
-  resources :users, only: [:index, :edit, :update]
-  resources :groups, only: [:new, :create, :edit, :update]
-  resources :messages, only: [:index, :create]
+# devise_for :users
+    root 'groups#index'   #ルートパスの指定
+ 	
+    resources :groups , only: [:new, :create, :edit, :update] do
+    	resources :messages, only: [:index, :create]
+    end
+    resources :users, only: [:edit, :update, :index] 
 end
