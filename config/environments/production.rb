ec6d07ac9eef097e6be1982109e0c722d9196734
近藤正道 <kuros96@icloud.comgit config --global user.name masamichi0963>
2019-12-15 04:12:18 +0900

    edit

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,6 @@
 Rails.application.configure do
+  server '<3.114.1.33>', user: 'ec2-user', roles: %w{app db web}
+
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
